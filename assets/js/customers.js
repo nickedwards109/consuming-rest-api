d11fa6e5c2f98b49994d46c1fc7a6979d67d6b2a
nickedwards109 <nickedwards109@gmail.com>
2018-11-04 19:16:08 -0500

    Get and display a customer's invoices
    When a user clicks on a customer card on the customers index page, the DOM nodes for all of the customers are removed from the DOM. The loading animation is appended to the DOM. The client queries the server for the invoices of the customer that was clicked. When the client receives the response, it generates a template with the customer's information as well as templates for each invoice, and it appends those templates to the DOM.

--- assets/js/customers.js
+++ assets/js/customers.js
@@ -1,6 +1,7 @@
 // Set a variable for the server endpoint and also a variable for a DOM node
 // that will contain a collection of nodes for displaying customer info.
-let url = "https://www.ecommerceapiexample.com/api/v1/customers";
+var url = "https://www.ecommerceapiexample.com/api/v1/customers";
+let body = $("body");
 let collectionContainer = $("#collection-container");
 
 // Query the server for data.
@@ -11,7 +12,7 @@ $.ajax({
     errorHandler(response);
   },
   success: function (response) {
-    successHandler(response);
+    allCustomersSuccessHandler(response);
   }
 });
 
@@ -23,16 +24,78 @@ function errorHandler(response) {
 
 // If the server responded with success, remove the loading animation from
 // the DOM. Display each customer's info in the DOM.
-function successHandler(response) {
+function allCustomersSuccessHandler(response) {
   console.log("The server returned a response with no errors.");
   $("#animation-container").remove();
   response.forEach(customer => {
-    var customerElement = `
-      <div class="customer">
+    var customerThumbnail = `
+      <div class="customer thumbnail" data-id=${customer.id} data-first-name=${customer.first_name} data-last-name=${customer.last_name}>
         <span>First Name:  ${customer.first_name}<span><br/>
         <span>Last Name:  ${customer.last_name}</span><br/>
 	<span>ID:  ${customer.id}</span><br/>
       </div>`;
-    collectionContainer.append(customerElement);
+    collectionContainer.append(customerThumbnail);
   })
+}
+
+// When the user clicks on a customer card:
+//   - Remove the display of every other customer
+//   - Show the loading animation
+//   - Call the handler which queries the server for the customer's invoices
+$(document).on("click", ".customer", function(event) {
+  collectionContainer.remove();
+  let loadingAnimation = `
+        <div id="animation-container">
+          <span>Loading!</span>
+          <div id="loading-animation"></div>
+        </div>
+      `;
+  body.append(loadingAnimation);
+  var customer = {
+    "firstName": this.getAttribute("data-first-name"),
+    "lastName": this.getAttribute("data-last-name"),
+    "id": this.getAttribute("data-id")
+  };
+  var url = `https://www.ecommerceapiexample.com/api/v1/customers/${customer.id}/invoices`;
+  $.ajax({
+    url: url,
+    type: "GET",
+    error: function(response) {
+      errorHandler(response);
+    },
+    success: function(response) {
+      singleCustomerSuccessHandler(customer, response);
+    }
+  });
+})
+
+//  This handler processes the response containing a customer's invoices.
+//   - Develop a markup template for each invoice
+//   - Remove the loading animation
+//   - Insert the invoice templates into the DOM
+function singleCustomerSuccessHandler(customer, response) {
+  var invoiceTemplates = response.map(invoice => {
+    var invoiceTemplate = `
+	  <div class="invoice thumbnail">
+            <span>Invoice ID: ${invoice.id}</span><br/>
+            <span>Merchant ID: ${invoice.merchant_id}</span><br/>
+            <span>Status: ${invoice.status}</span><br/>
+          </div>
+        `;
+    return invoiceTemplate;
+  });
+  $("#animation-container").remove();
+	console.log(invoiceTemplates);
+  var customerDataSheetTemplate = `
+    <div class="customer-data-sheet collection-container">
+        <span>First Name:  ${customer.firstName}<span><br/>
+        <span>Last Name:  ${customer.lastName}</span><br/>
+        <span>ID:  ${customer.id}</span><br/>
+    </div>
+  `;
+  body.append(customerDataSheetTemplate);
+  var customerDataSheet = $(".customer-data-sheet");
+  invoiceTemplates.forEach(invoiceTemplate => {
+    customerDataSheet.append(invoiceTemplate);
+  });
 }
