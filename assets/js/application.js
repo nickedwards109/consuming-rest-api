cbaa845a0b792499c77a32958b8213cfb9b5b33e
nickedwards109 <nickedwards109@gmail.com>
2018-11-05 22:05:34 -0500

    Implement basic routing
    This commit implements the following user stories:
    
    As a user
    When I visit the customers index page
    And I click on a customer card
      I see the URL change to show the ID of that customer
      I see a page indicating the invoices of that customer
    When I click on the browser back button
      I am taken back to the customers index page
      I see the URL change to no longer show the ID of that customer
    
    As a user
    Whenever I navigate between pages
      I see a loading animation while the page loads

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,20 +1,70 @@
-// Set a variable for the server endpoint and also a variable for a DOM node
-// that will contain a collection of nodes for displaying customer info.
-var url = "https://www.ecommerceapiexample.com/api/v1/customers";
+// Get the body tag so that templates can be appended to it.
 let body = $("body");
-let collectionContainer = $("#collection-container");
 
-// Query the server for data.
-$.ajax({
-  url: url,
-  type: "GET",
-  error: function (response) {
-    errorHandler(response);
-  },
-  success: function (response) {
-    allCustomersSuccessHandler(response);
-  }
-});
+// Generate a template for a loading animation that can be reused anywhere
+let loadingAnimation = `
+      <div id="animation-container">
+        <span>Loading!</span>
+        <div id="loading-animation"></div>
+      </div>
+    `;
+
+// Generate a template for a container for a collection of many templates.
+// For example, this could contain many templates that each represent a customer.
+var collectionContainer = $(`<div id="collection-container"></div>`);
+
+// Get the current path. This will be needed later for routing purposes.
+var path = window.location;
+
+// Add /customers to the current path, since the first page displayed is
+// the customers page.
+window.location = path + '#customers';
+
+// These are the set of possible URLs that a user might enter into the
+// browser bar or that the user might be redirected to (still without a
+// new page load)
+let routes = [
+  { pathMatcher: /index.html#customers$/, name: "customersIndex" },
+  { pathMatcher: /index.html#customers\/[0-9]+/, name: "customerShow" }
+];
+
+// Match the current path with a route and call the appropriate action
+match(path, routes);
+
+// Define the function that matches a route with a path and calls an action
+function match(path, routes) {
+  routes.forEach(route => {
+  var isMatch = route.pathMatcher.test(path);
+      // If the user is navigating to the customers index page:
+      //   - Remove all content from the page
+      //   - Display the loading animation
+      //   - Call the action for getting and displaying all the customers' data
+      if (isMatch === true && route.name === "customersIndex") {
+      $("body").empty();
+      $("body").append(loadingAnimation);
+      getCustomers();
+    }
+  });
+}
+
+// When the URL changes, match the new URL with a route and call the
+// appropriate action
+$(window).on('popstate', (event) => {
+  match(path, routes);
+} );
+
+function getCustomers() {
+  $.ajax({
+    url: "https://www.ecommerceapiexample.com/api/v1/customers",
+    type: "GET",
+    error: function (response) {
+      errorHandler(response);
+    },
+    success: function (response) {
+      allCustomersSuccessHandler(response);
+    }
+  });
+}
 
 // If the server responded with an error, display the error in the console.
 function errorHandler(response) {
@@ -27,7 +77,9 @@ function errorHandler(response) {
 function allCustomersSuccessHandler(response) {
   console.log("The server returned a response with no errors.");
   $("#animation-container").remove();
-  response.forEach(customer => {
+  if ($(".customer-data-sheet")) {$(".customer-data-sheet").remove();}
+    body.append(collectionContainer); 
+    response.forEach(customer => {
     var customerThumbnail = `
       <div class="customer thumbnail" data-id=${customer.id} data-first-name=${customer.first_name} data-last-name=${customer.last_name}>
         <span>First Name:  ${customer.first_name}<span><br/>
@@ -44,12 +96,6 @@ function allCustomersSuccessHandler(response) {
 //   - Call the handler which queries the server for the customer's invoices
 $(document).on("click", ".customer", function(event) {
   collectionContainer.remove();
-  let loadingAnimation = `
-        <div id="animation-container">
-          <span>Loading!</span>
-          <div id="loading-animation"></div>
-        </div>
-      `;
   body.append(loadingAnimation);
   var customer = {
     "firstName": this.getAttribute("data-first-name"),
@@ -70,11 +116,14 @@ $(document).on("click", ".customer", function(event) {
 })
 
 //  This handler processes the response containing a customer's invoices.
+//   - Update the URL to the show page for the current customer
 //   - Develop a markup template for each invoice
 //   - Remove the loading animation
 //   - Insert the invoice templates into the DOM
 function singleCustomerSuccessHandler(customer, response) {
-  var invoiceTemplates = response.map(invoice => {
+    window.location = window.location + `/${customer.id}`;
+    match(path, routes);
+    var invoiceTemplates = response.map(invoice => {
     var invoiceTemplate = `
 	  <div class="invoice thumbnail">
             <span>Invoice ID: ${invoice.id}</span><br/>
@@ -85,7 +134,7 @@ function singleCustomerSuccessHandler(customer, response) {
     return invoiceTemplate;
   });
   $("#animation-container").remove();
-	console.log(invoiceTemplates);
+  $("#collection-container").remove();
   var customerDataSheetTemplate = `
     <div class="customer-data-sheet collection-container">
         <span>First Name:  ${customer.firstName}<span><br/>
